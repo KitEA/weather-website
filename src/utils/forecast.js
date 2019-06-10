c9d64105c88d1d33bc6cfb39f4ff6a24d2f98889
Kit_EA <kit.earf@gmail.com>
2019-06-10 19:59:23 +0500

    Add additional info to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -11,7 +11,7 @@ const forecast = (latitude, longitude, callback) => {
       callback('Unable to find location', undefined)
     } else {
       callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + 
-        ' degrees out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+        ' degrees out. Minimum temperature is ' + body.daily.data[0].temperatureMin + ' degrees and maximum is ' + body.daily.data[0].temperatureMax + ' degrees. There is a ' + body.currently.precipProbability + '% chance of rain.')
     }
   })
 }
